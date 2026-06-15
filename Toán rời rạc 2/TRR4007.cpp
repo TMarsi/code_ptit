5796cf141d8feb14460344c83434f945270f1a1c
TMarsi <122539045+TMarsi@users.noreply.github.com>
2026-06-15 21:44:04 +0700

    Update TRR4007.cpp

--- Toán rời rạc 2/TRR4007.cpp
+++ Toán rời rạc 2/TRR4007.cpp
@@ -1,65 +1,45 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-struct Edge {
-    int u, v, w;
+int n,m,c[101],sum = 0;
+struct edge{
+	int u,v,w;
 };
+vector<edge> edges;
+vector<edge> kq;
 
-struct DSU {
-    vector<int> parent, rank_;
-    DSU(int n) : parent(n+1), rank_(n+1, 0) {
-        iota(parent.begin(), parent.end(), 0);
+int find(int x){
+	if(c[x] != x) c[x] = find(c[x]);
+	return c[x];
+} 
+
+
+
+int main(){
+	freopen("CK.INP","r",stdin);
+	freopen("CK.OUT","w",stdout);
+
+	cin >> n >> m;
+	edges.resize(m);
+	for(int i = 0;i < m;i++){
+		cin >> edges[i].u >> edges[i].v >> edges[i].w;
+	}
+	stable_sort(edges.begin(),edges.end(),[](const edge &a, const edge &b) {return a.w < b.w;});
+	for(int i = 1;i <= n;i++) c[i] = i;
+	for(int i = 0; i < (int)edges.size(); i++){
+    int ru = find(edges[i].u);
+    int rv = find(edges[i].v);
+    if(ru != rv){
+        c[ru] = rv;
+        sum += edges[i].w;
+        kq.push_back(edges[i]);
     }
-    int find(int x) {
-        if (parent[x] != x) parent[x] = find(parent[x]);
-        return parent[x];
-    }
-    bool unite(int x, int y) {
-        x = find(x); y = find(y);
-        if (x == y) return false;
-        if (rank_[x] < rank_[y]) swap(x, y);
-        parent[y] = x;
-        if (rank_[x] == rank_[y]) rank_[x]++;
-        return true;
-    }
-};
-
-int main() {
-    freopen("CK.INP", "r", stdin);
-    freopen("CK.OUT", "w", stdout);
-
-    int n, m;
-    cin >> n >> m;
-
-    vector<Edge> edges(m);
-    for (int i = 0; i < m; i++)
-        cin >> edges[i].u >> edges[i].v >> edges[i].w;
-
-    
-    stable_sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b) {
-        return a.w < b.w;
-    });
-
-    DSU dsu(n);
-    int totalWeight = 0;
-    int edgesUsed = 0;
-    vector<Edge> mstEdges;
-
-    for (auto& e : edges) {
-        if (dsu.unite(e.u, e.v)) {
-            totalWeight += e.w;
-            mstEdges.push_back(e);
-            edgesUsed++;
-            if (edgesUsed == n - 1) break;
-        }
-    }
-
-    if (edgesUsed < n - 1) {
-        cout << 0 << "\n";
+}
+	if ((int)kq.size() < n - 1) {
+        cout << 0;
     } else {
-        cout << totalWeight << "\n";
-        for (auto& e : mstEdges)
+        cout << sum << "\n";
+        for (auto &e : kq)
             cout << e.u << " " << e.v << " " << e.w << "\n";
     }
-    return 0;
 }
