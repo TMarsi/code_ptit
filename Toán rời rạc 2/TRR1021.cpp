fc420bf45857a109dfe29f48fd959ed70dce5a4d
TMarsi <122539045+TMarsi@users.noreply.github.com>
2026-06-15 22:03:26 +0700

    Update TRR1021.cpp

--- Toán rời rạc 2/TRR1021.cpp
+++ Toán rời rạc 2/TRR1021.cpp
@@ -1,42 +1,39 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-const int INF = 10000;
-
-int main() {
-    freopen("DT.INP", "r", stdin);
-    freopen("DT.OUT", "w", stdout);
-
-    int t, n;
-    cin >> t >> n;
-
-    vector<vector<int>> c(n+1, vector<int>(n+1, INF));
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            cin >> c[i][j];
-
-    vector<int> indeg(n+1, 0), outdeg(n+1, 0);
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            if (i != j && c[i][j] < INF) {
-                outdeg[i]++;
-                indeg[j]++;
-            }
-
-    if (t == 1) {
-        for (int i = 1; i <= n; i++)
-            cout << indeg[i] << " " << outdeg[i] << "\n";
-    } else {
-        
-        vector<tuple<int,int,int>> edges;
-        for (int u = 1; u <= n; u++)
-            for (int v = 1; v <= n; v++)
-                if (u != v && c[u][v] < INF)
-                    edges.push_back({u, v, c[u][v]});
-        sort(edges.begin(), edges.end());
-        cout << n << " " << edges.size() << "\n";
-        for (auto& [u, v, w] : edges)
-            cout << u << " " << v << " " << w << "\n";
-    }
-    return 0;
+int n,m,a[101][101],degIn[101],degOut[101],b[101][101];
+pair<int,int> edges[101];
+int main(){
+	freopen("DT.INP","r",stdin);
+	freopen("DT.OUT","w",stdout);
+	m = 0;
+	int t;cin >> t;
+	cin >> n;
+	for(int i = 1;i <= n;i++){
+		for(int j = 1;j <= n;j++){
+			cin >> a[i][j];
+			if(i != j && a[i][j] != 0 && a[i][j] != 10000){
+				degIn[j]++;
+				degOut[i]++;
+			}
+		}
+	}
+	if(t == 1){
+		for(int i = 1;i <= n;i++){
+			cout << degIn[i] << " " <<degOut[i] << "\n";
+		}
+	} else {
+		for(int i = 1;i <= n;i++){
+			for(int j = 1;j <= n;j++){
+				if(a[i][j] != 0 && a[i][j] != 10000 && i != j) m++;
+			}
+		}
+		cout << n << " " << m << "\n";
+		for(int i = 1;i <= m;i++){
+			for(int j = 1;j <= n;j++){
+				if(a[i][j] != 0 && a[i][j] != 10000 && i != j) cout << i << " "  <<  j << " " << a[i][j] << "\n";
+			}
+		}
+	}
+	return 0;
 }
