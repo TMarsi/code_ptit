a1b99ea3709a419ec676805feed013529162150c
TMarsi <122539045+TMarsi@users.noreply.github.com>
2026-06-15 21:43:03 +0700

    Update TRR5007.cpp

--- Toán rời rạc 2/TRR5007.cpp
+++ Toán rời rạc 2/TRR5007.cpp
@@ -1,79 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 const int INF = 10000;
+int n, s, t, c[105][105];
+int a[105], path[105];
 
 int main() {
     freopen("BN.INP", "r", stdin);
     freopen("BN.OUT", "w", stdout);
 
-    int n, s, t;
     cin >> n >> s >> t;
-
-    vector<vector<int>> c(n+1, vector<int>(n+1, INF));
-    for (int i = 1; i <= n; i++) c[i][i] = 0;
     for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            cin >> c[i][j];
+        for (int j = 1; j <= n; j++) cin >> c[i][j];
 
-    
-    vector<tuple<int,int,int>> edges;
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            if (i != j && c[i][j] < INF)
-                edges.push_back({i, j, c[i][j]});
+    for (int i = 1; i <= n; i++) { a[i] = INF; path[i] = -1; }
+    a[s] = 0;
 
-    vector<long long> dist(n+1, LLONG_MAX);
-    vector<int> parent(n+1, -1);
-    dist[s] = 0;
-
-    
-    for (int iter = 0; iter < n - 1; iter++) {
-        bool updated = false;
-        for (auto& [u, v, w] : edges) {
-            if (dist[u] != LLONG_MAX && dist[u] + w < dist[v]) {
-                dist[v] = dist[u] + w;
-                parent[v] = u;
-                updated = true;
+    for (int i = 1; i < n ; i++) {
+        for (int u = 1; u <= n; u++) {
+            if (a[u] == INF) continue;
+            for (int v = 1; v <= n; v++) {
+                if (u != v && c[u][v] != INF && a[u] + c[u][v] < a[v]) {
+                    a[v] = a[u] + c[u][v];
+                    path[v] = u;
+                }
             }
         }
-        if (!updated) break;
-    }
-
-    
-    
-    
-    vector<bool> inNegCycle(n+1, false);
-    for (auto& [u, v, w] : edges) {
-        if (dist[u] != LLONG_MAX && dist[u] + w < dist[v]) {
-            inNegCycle[v] = true;
-        }
     }
     
-    for (int iter = 0; iter < n; iter++) {
-        for (auto& [u, v, w] : edges) {
-            if (inNegCycle[u]) inNegCycle[v] = true;
+    bool ok = false;
+    for (int u = 1; u <= n; u++) {
+        if (a[u] == INF) continue;
+        for (int v = 1; v <= n; v++) {
+            if (u != v && c[u][v] != INF && a[u] + c[u][v] < a[v])
+                ok = true;
         }
     }
 
-    if (inNegCycle[t]) {
-        cout << -1 << "\n";
-    } else if (dist[t] == LLONG_MAX) {
-        cout << 0 << "\n";
+    if (ok) {
+        cout << -1;
+    } else if (a[t] == INF) {
+        cout << 0;
     } else {
-        cout << dist[t] << "\n";
-        vector<int> path;
-        int cur = t;
-        while (cur != -1) {
-            path.push_back(cur);
-            cur = parent[cur];
-        }
-        reverse(path.begin(), path.end());
-        for (int i = 0; i < (int)path.size(); i++) {
-            if (i > 0) cout << " ";
-            cout << path[i];
-        }
-        cout << "\n";
+        cout << a[t] << "\n";
+        vector<int> par;
+        for (int x = t; x != -1; x = path[x]) par.push_back(x);
+        reverse(par.begin(), par.end());
+        for (int i = 0; i < (int)par.size(); i++)
+            cout << par[i] << (i + 1 < (int)par.size() ? " " : "\n");
     }
     return 0;
 }
