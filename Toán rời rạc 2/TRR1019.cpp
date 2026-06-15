5fe21604affc1cae3c54303e564811bf36371b92
TMarsi <122539045+TMarsi@users.noreply.github.com>
2026-06-15 22:02:42 +0700

    Refactor input handling and degree calculation

--- Toán rời rạc 2/TRR1019.cpp
+++ Toán rời rạc 2/TRR1019.cpp
@@ -1,42 +1,38 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    freopen("DT.INP", "r", stdin);
-    freopen("DT.OUT", "w", stdout);
+int n,m,a[101][101],degIn[101],degOut[101];
+int main(){
+	freopen("DT.INP","r",stdin);
+	freopen("DT.OUT","w",stdout);
 
-    int t, n;
-    cin >> t >> n;
-
-    vector<vector<int>> a(n+1, vector<int>(n+1, 0));
-    vector<int> indeg(n+1, 0), outdeg(n+1, 0);
-
-    for (int i = 1; i <= n; i++) {
-        int k;
-        cin >> k;
-        for (int j = 0; j < k; j++) {
-            int v;
-            cin >> v;
-            a[i][v] = 1;
-            outdeg[i]++;
-            indeg[v]++;
-        }
-    }
-
-    if (t == 1) {
-        for (int i = 1; i <= n; i++)
-            cout << indeg[i] << " " << outdeg[i] << "\n";
-    } else {
-        
-        vector<pair<int,int>> edges;
-        for (int u = 1; u <= n; u++)
-            for (int v = 1; v <= n; v++)
-                if (a[u][v])
-                    edges.push_back({u, v});
-        sort(edges.begin(), edges.end());
-        cout << n << " " << edges.size() << "\n";
-        for (auto& e : edges)
-            cout << e.first << " " << e.second << "\n";
-    }
-    return 0;
+	int t;cin >> t;
+	cin >> n;
+	for(int i = 1;i <= n;i++){
+		int k;cin >>k;
+		degOut[i] = k;
+		for(int j = 1;j <= k;j++){
+			int u;cin >> u;
+			a[i][u] = 1;
+			degIn[u]++;
+		}
+	}
+	if(t == 1){
+		for(int i = 1;i <= n;i++){
+			cout << degIn[i] << " " << degOut[i] << "\n";
+		}
+	}else{
+		for(int i = 1;i <= n;i++){
+			for(int j = 1;j <= n;j++){
+				if(a[i][j]) m++;
+			}
+		}
+		cout << n << " " << m << "\n";
+		for(int i = 1;i <= m;i++){
+			for(int j = 1;j <= n;j++){
+				if(a[i][j]) cout << i << " " << j <<"\n";
+			}
+		}
+	}
+	return 0;
 }
