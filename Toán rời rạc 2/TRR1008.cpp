520212068b70e2832005e4ea92917698acd465b3
TMarsi <122539045+TMarsi@users.noreply.github.com>
2026-06-15 21:57:44 +0700

    Update TRR1008.cpp

--- Toán rời rạc 2/TRR1008.cpp
+++ Toán rời rạc 2/TRR1008.cpp
@@ -1,47 +1,38 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    freopen("DT.INP", "r", stdin);
-    freopen("DT.OUT", "w", stdout);
+int n,m,k,a[101][101],deg[101],eu[1001],ev[1001];
 
-    int t, n;
-    cin >> t >> n;
-
-    vector<vector<int>> a(n+1, vector<int>(n+1, 0));
-
-    for (int i = 1; i <= n; i++) {
-        int k;
-        cin >> k;
-        for (int j = 0; j < k; j++) {
-            int v;
-            cin >> v;
-            a[i][v] = 1;
-            a[v][i] = 1;
-        }
-    }
-
-    vector<int> deg(n+1, 0);
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            if (a[i][j]) deg[i]++;
-
-    if (t == 1) {
-        for (int i = 1; i <= n; i++) {
-            if (i > 1) cout << " ";
-            cout << deg[i];
-        }
-        cout << "\n";
-    } else {
-        
-        vector<pair<int,int>> edges;
-        for (int u = 1; u <= n; u++)
-            for (int v = u+1; v <= n; v++)
-                if (a[u][v])
-                    edges.push_back({u, v});
-        cout << n << " " << edges.size() << "\n";
-        for (auto& e : edges)
-            cout << e.first << " " << e.second << "\n";
-    }
-    return 0;
+int main(){
+	freopen("DT.INP","r",stdin);
+    freopen("DT.OUT","w",stdout);
+	int t;cin >> t;
+	cin >> n;
+	for(int i = 1;i <= n;i++){
+		cin >> k;
+		while(k--){
+			int u;cin >> u;
+			deg[u]++;
+			a[u][i] = 1;
+			a[i][u] = 1;
+		}
+	}
+	if(t == 1){
+		for(int i = 1;i <= n;i++){
+			cout << deg[i] << " ";
+		}
+	} else {
+		m = 0;
+		for(int i = 1;i <= n;i++){
+			for(int j = i + 1;j <= n;j++){
+				if(a[i][j]) m++;
+			}
+		}
+		cout << n << " " << m << "\n";
+		for(int i = 1;i <= n;i++){
+			for(int j = i + 1;j <= n;j++){
+				if(a[i][j]) cout << i << " " << j <<"\n";
+			}
+		}
+	}
 }
