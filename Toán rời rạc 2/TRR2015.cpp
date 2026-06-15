4c1c61612293a1d898bd71ed220587e72e67c68e
TMarsi <122539045+TMarsi@users.noreply.github.com>
2026-06-15 21:51:09 +0700

    Update TRR2015.cpp

--- Toán rời rạc 2/TRR2015.cpp
+++ Toán rời rạc 2/TRR2015.cpp
@@ -1,79 +1,72 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int n;
-vector<vector<int>> a;
+int n,a[101][101],c[101][101];
+bool vs[101];
+vector<int> edges;
+int sodinhtham;
 
-void dfs_directed(int u, vector<bool>& visited) {
-    visited[u] = true;
-    for (int v = 1; v <= n; v++)
-        if (a[u][v] && !visited[v])
-            dfs_directed(v, visited);
+void DFS(int x){
+	vs[x] = true;
+	sodinhtham++;
+	for(int i = 1;i <= n;i++){
+		if(c[x][i] == 1 && !vs[i]){
+			DFS(i);
+		}
+	}
 }
 
-void dfs_undirected(int u, vector<bool>& visited) {
-    visited[u] = true;
-    for (int v = 1; v <= n; v++)
-        if ((a[u][v] || a[v][u]) && !visited[v])
-            dfs_undirected(v, visited);
+bool thamdinh(){
+	for(int i = 1;i <= n;i++) vs[i]= false;
+	sodinhtham = 0;
+	
+	DFS(1);
+	return (sodinhtham == n);
 }
 
-int main() {
-    freopen("TK.INP", "r", stdin);
-    freopen("TK.OUT", "w", stdout);
+int main(){
+	freopen("TK.INP","r",stdin);
+	freopen("TK.OUT","w",stdout);
+	cin >> n;
+	for(int i = 1;i <= n;i++){
+		for(int j = 1;j <= n;j++){
+			cin >> a[i][j];
+			c[i][j] = a[i][j];	
+		} 
+	}
 
-    cin >> n;
-    a.assign(n+1, vector<int>(n+1, 0));
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            cin >> a[i][j];
+	bool thuan = thamdinh();
 
-    
-    vector<bool> vis(n+1, false);
-    dfs_undirected(1, vis);
-    bool weakly = true;
-    for (int i = 1; i <= n; i++)
-        if (!vis[i]) { weakly = false; break; }
+	for(int i = 1;i <= n;i++){
+		for(int j = 1;j <= n;j++) c[i][j] = a[j][i];
+	}
+	
+	bool nguoc = thamdinh();
 
-    if (!weakly) {
-        cout << 0 << "\n";
-        return 0;
-    }
+	if(thuan && nguoc){
+		cout << 1;
+		return 0;
+	}
 
-    
-    fill(vis.begin(), vis.end(), false);
-    dfs_directed(1, vis);
-    bool fwd = true;
-    for (int i = 1; i <= n; i++)
-        if (!vis[i]) { fwd = false; break; }
-
-    if (!fwd) {
-        cout << 2 << "\n";
-        return 0;
-    }
-
-    
-    
-    fill(vis.begin(), vis.end(), false);
-    vis[1] = true;
-    
-    stack<int> st;
-    st.push(1);
-    while (!st.empty()) {
-        int u = st.top(); st.pop();
-        for (int v = 1; v <= n; v++) {
-            if (a[v][u] && !vis[v]) {
-                vis[v] = true;
-                st.push(v);
-            }
+	for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            c[i][j] = 0;
         }
     }
-    bool bwd = true;
-    for (int i = 1; i <= n; i++)
-        if (!vis[i]) { bwd = false; break; }
 
-    if (fwd && bwd) cout << 1 << "\n";
-    else cout << 2 << "\n";
+	for(int i = 1;i <= n;i++){
+		for(int j = 1;j <= n;j++){
+			if(a[i][j] == 1){
+				c[i][j] = 1;
+				c[j][i] = 1;
+			}
+		}
+	}
 
-    return 0;
+	bool yeu = thamdinh();
+
+	if(yeu) cout << 2;
+	else cout << 0;
+
+	return  0;
 }
