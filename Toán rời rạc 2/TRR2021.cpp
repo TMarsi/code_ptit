a52fb2ab44eb2b12d2965277c00d2208dd0d8166
TMarsi <122539045+TMarsi@users.noreply.github.com>
2026-06-15 21:50:45 +0700

    Update TRR2021.cpp

--- Toán rời rạc 2/TRR2021.cpp
+++ Toán rời rạc 2/TRR2021.cpp
@@ -1,70 +1,47 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int n;
-vector<vector<int>> a;
-vector<int> disc, low, parent_arr;
-vector<bool> visited;
-vector<bool> isAP;
-int timer_cnt;
+int n,a[101][101];
+bool vs[101];
 
-void dfs(int u) {
-    visited[u] = true;
-    disc[u] = low[u] = timer_cnt++;
-    int children = 0;
-
-    for (int v = 1; v <= n; v++) {
-        if (!a[u][v]) continue;
-        if (!visited[v]) {
-            children++;
-            parent_arr[v] = u;
-            dfs(v);
-            low[u] = min(low[u], low[v]);
-            
-            
-            if (parent_arr[u] == -1 && children > 1)
-                isAP[u] = true;
-            
-            if (parent_arr[u] != -1 && low[v] >= disc[u])
-                isAP[u] = true;
-        } else if (v != parent_arr[u]) {
-            low[u] = min(low[u], disc[v]);
-        }
-    }
+void DFS(int x){
+	vs[x] = true;
+	for(int i = 1;i <= n;i++){
+		if(a[x][i] == 1 && !vs[i]) {
+			DFS(i);
+		}
+	}
 }
 
-int main() {
-    freopen("TK.INP", "r", stdin);
-    freopen("TK.OUT", "w", stdout);
 
-    cin >> n;
-    a.assign(n+1, vector<int>(n+1, 0));
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            cin >> a[i][j];
+int main(){
+	freopen("TK.INP","r",stdin);
+	freopen("TK.OUT","w",stdout);
 
-    disc.assign(n+1, 0);
-    low.assign(n+1, 0);
-    parent_arr.assign(n+1, -1);
-    visited.assign(n+1, false);
-    isAP.assign(n+1, false);
-    timer_cnt = 0;
+	cin >> n;
+	for(int i = 1;i <= n;i++){
+		for(int j = 1;j <= n;j++) cin >> a[i][j];
+	}
+	
+	vector<int> ans;
+	for(int v = 1;v <= n;v++){
+		memset(vs,false,sizeof(vs));
+		vs[v] = true;
+		int cnt = 0;
+		for(int i = 1;i <= n;i++){
+			if(a[v][i] == 1 && !vs[i]){
+				DFS(i);
+				cnt++;
+			}
+		}
+		if(cnt >= 2) ans.push_back(v); 
+	}
+	cout << ans.size() << "\n";
+	if(!ans.empty()){
+		for(int i = 0;i < (int)ans.size();i++){
+			cout << ans[i] << (i < (int)ans.size() - 1 ? " " : "\n");
+		}	
+	}
+	
 
-    for (int i = 1; i <= n; i++)
-        if (!visited[i])
-            dfs(i);
-
-    vector<int> result;
-    for (int i = 1; i <= n; i++)
-        if (isAP[i]) result.push_back(i);
-
-    cout << result.size() << "\n";
-    if (!result.empty()) {
-        for (int i = 0; i < (int)result.size(); i++) {
-            if (i > 0) cout << " ";
-            cout << result[i];
-        }
-        cout << "\n";
-    }
-    return 0;
 }
