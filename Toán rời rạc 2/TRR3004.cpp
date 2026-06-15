73f19e334ac178f0baee45d1c417326c0a4f1e3e
TMarsi <122539045+TMarsi@users.noreply.github.com>
2026-06-15 21:48:22 +0700

    Update TRR3004.cpp

--- Toán rời rạc 2/TRR3004.cpp
+++ Toán rời rạc 2/TRR3004.cpp
@@ -1,128 +1,76 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int n;
-vector<vector<int>> a;
+int n,u,a[101][101],degOut[101],degIn[101];
+bool vs[101];
+vector<int> ans;
 
-void dfs_directed(int u, vector<bool>& vis) {
-    vis[u] = true;
-    for (int v = 1; v <= n; v++)
-        if (a[u][v] && !vis[v])
-            dfs_directed(v, vis);
+void DFS(int x){
+	vs[x] = true;
+	for(int i = 1;i <= n;i++){
+		if((a[x][i] == 1 || a[i][x] == 1) && !vs[i]){
+			DFS(i);
+		}
+	}
 }
 
-void dfs_undirected(int u, vector<bool>& vis) {
-    vis[u] = true;
-    for (int v = 1; v <= n; v++)
-        if ((a[u][v] || a[v][u]) && !vis[v])
-            dfs_undirected(v, vis);
+void find(int x){
+	for(int i = 1;i <= n;i++){
+		if(a[x][i] == 1){
+			a[x][i] = 0;
+			find(i);
+		}
+	}
+	ans.push_back(x);
 }
 
-bool isStronglyConnected() {
-    vector<bool> vis(n+1, false);
-    dfs_directed(1, vis);
-    for (int i = 1; i <= n; i++) if (!vis[i]) return false;
-    
-    fill(vis.begin(), vis.end(), false);
-    vis[1] = true;
-    stack<int> st;
-    st.push(1);
-    while (!st.empty()) {
-        int u = st.top(); st.pop();
-        for (int v = 1; v <= n; v++)
-            if (a[v][u] && !vis[v]) { vis[v] = true; st.push(v); }
-    }
-    for (int i = 1; i <= n; i++) if (!vis[i]) return false;
-    return true;
-}
+int main(){
+	freopen("CT.INP","r",stdin);
+	freopen("CT.OUT","w",stdout);
 
-bool isWeaklyConnected() {
-    vector<bool> vis(n+1, false);
-    dfs_undirected(1, vis);
-    for (int i = 1; i <= n; i++) if (!vis[i]) return false;
-    return true;
-}
+	int t;cin >> t;
+	if(t == 1){
+		cin >> n;
+		for(int i = 1;i <= n;i++){
+			for(int j = 1;j <= n;j++){
+				cin >> a[i][j];
+				if(a[i][j]) {
+					degOut[i]++;
+					degIn[j]++;
+				}
+			}
+		}
 
-vector<int> hierholzer(int start) {
-    vector<vector<int>> rem = a;
-    stack<int> st;
-    vector<int> circuit;
-    st.push(start);
-    while (!st.empty()) {
-        int u = st.top();
-        bool found = false;
-        for (int v = 1; v <= n; v++) {
-            if (rem[u][v] > 0) {
-                rem[u][v]--;
-                st.push(v);
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            circuit.push_back(u);
-            st.pop();
-        }
-    }
-    reverse(circuit.begin(), circuit.end());
-    return circuit;
-}
+		DFS(1);
+		bool ok = true;
+		for(int i = 1;i <= n;i++){
+			if(!vs[i]) {
+				ok = false;
+				break;
+			}
+		}
+		int dau = 0,cuoi = 0,mid = 0;
+		for(int i = 1;i <= n;i++){
+			int diff = degOut[i] - degIn[i];
+			if(diff == 1) dau++;
+			else if(diff == -1) cuoi++;
+			else if(diff != 0) mid++;
 
-int main() {
-    freopen("CT.INP", "r", stdin);
-    freopen("CT.OUT", "w", stdout);
-
-    int t;
-    cin >> t;
-
-    if (t == 1) {
-        cin >> n;
-        a.assign(n+1, vector<int>(n+1, 0));
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= n; j++)
-                cin >> a[i][j];
-
-        vector<int> indeg(n+1, 0), outdeg(n+1, 0);
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= n; j++)
-                if (a[i][j]) { outdeg[i]++; indeg[j]++; }
-
-        
-        if (isStronglyConnected()) {
-            bool euler = true;
-            for (int i = 1; i <= n; i++)
-                if (indeg[i] != outdeg[i]) { euler = false; break; }
-            if (euler) { cout << 1 << "\n"; return 0; }
-        }
-
-        
-        if (isWeaklyConnected()) {
-            int cntOut = 0, cntIn = 0, cntBal = 0;
-            for (int i = 1; i <= n; i++) {
-                if (outdeg[i] == indeg[i] + 1) cntOut++;
-                else if (indeg[i] == outdeg[i] + 1) cntIn++;
-                else if (indeg[i] == outdeg[i]) cntBal++;
-            }
-            if (cntOut == 1 && cntIn == 1 && cntBal == n - 2) {
-                cout << 2 << "\n"; return 0;
-            }
-        }
-
-        cout << 0 << "\n";
-    } else {
-        int u;
-        cin >> n >> u;
-        a.assign(n+1, vector<int>(n+1, 0));
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= n; j++)
-                cin >> a[i][j];
-
-        vector<int> circuit = hierholzer(u);
-        for (int i = 0; i < (int)circuit.size(); i++) {
-            if (i > 0) cout << " ";
-            cout << circuit[i];
-        }
-        cout << "\n";
-    }
-    return 0;
+		}
+		if(ok && dau == 0 && mid == 0 && cuoi == 0) cout << 1;
+		else if(ok && dau == 1 && cuoi == 1 && mid == 0) cout << 2;
+		else cout << 0;
+	} else {
+		cin >> n >> u;
+		for(int i = 1;i <= n;i++){
+			for(int j = 1;j <= n;j++){
+				cin >> a[i][j];
+			}
+		}
+		find(u);
+		reverse(ans.begin(),ans.end());
+		for(int i = 0;i < (int) ans.size();i++){
+			cout << ans[i] << (i < (int)ans.size() - 1 ? " " : "\n");
+		}
+	}
 }
