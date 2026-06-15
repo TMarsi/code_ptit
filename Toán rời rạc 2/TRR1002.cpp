607ccfb82ce649716ae3f87f7f4e865c9a3e4d4b
TMarsi <122539045+TMarsi@users.noreply.github.com>
2026-06-15 21:55:24 +0700

    Refactor main function and input handling

--- Toán rời rạc 2/TRR1002.cpp
+++ Toán rời rạc 2/TRR1002.cpp
@@ -1,41 +1,35 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    freopen("DT.INP", "r", stdin);
-    freopen("DT.OUT", "w", stdout);
+int n,a[101][101],deg[101];
 
-    int t, n;
-    cin >> t >> n;
-
-    vector<vector<int>> a(n+1, vector<int>(n+1, 0));
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            cin >> a[i][j];
-
-    vector<int> deg(n+1, 0);
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            if (a[i][j]) deg[i]++;
-
-    if (t == 1) {
-        for (int i = 1; i <= n; i++) {
-            if (i > 1) cout << " ";
-            cout << deg[i];
-        }
-        cout << "\n";
-    } else {
-        
-        cout << n << "\n";
-        for (int i = 1; i <= n; i++) {
-            vector<int> neighbors;
-            for (int j = 1; j <= n; j++)
-                if (a[i][j]) neighbors.push_back(j);
-            sort(neighbors.begin(), neighbors.end());
-            cout << neighbors.size();
-            for (int v : neighbors) cout << " " << v;
-            cout << "\n";
-        }
-    }
-    return 0;
+int main(){
+	freopen("DT.INP","r",stdin);
+	freopen("DT.OUT","w",stdout);
+	
+	int t;cin >> t;
+	cin >> n;
+	for(int i = 1;i <= n;i++){
+		for(int j = 1;j <= n;j++) {
+			cin >> a[i][j];
+			if(a[i][j]) deg[i]++;
+		}
+	}
+	if(t == 1){
+		for(int i = 1;i <= n;i++){
+			cout << deg[i] << " ";
+		}
+	} else {
+		cout << n <<"\n";
+		for(int i = 1;i <= n;i++){
+			cout << deg[i] << " ";
+			for(int j = 1;j <= n;j++){
+				if(a[i][j]){
+					cout << j << " ";
+				}
+				if(j == n) cout << "\n";
+			}
+		}
+	}
+	return 0;
 }
