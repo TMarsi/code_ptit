7df40999f9c96495fc5909e206a92f52de6a62d6
TMarsi <122539045+TMarsi@users.noreply.github.com>
2026-06-15 21:59:51 +0700

    Update TRR1013.cpp

--- Toán rời rạc 2/TRR1013.cpp
+++ Toán rời rạc 2/TRR1013.cpp
@@ -1,45 +1,37 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-  freopen("DT.INP", "r", stdin);
-  freopen("DT.OUT", "w", stdout);
+int n,m,a[101][101],degIn[101],degOut[101];
+int main(){
+	freopen("DT.INP","r",stdin);
+	freopen("DT.OUT","w",stdout);
 
-  int t, n;
-  cin >> t >> n;
+	int t;cin >> t;
+	cin >> n;
+	for(int i = 1;i <= n;i++){
+		for(int j = 1;j <= n;j++){
+			cin >> a[i][j];
+			if(a[i][j]){
+				degOut[i]++;
+				degIn[j]++;
+			}
+		}
+	}
 
-  vector<vector<int>> a(n + 1, vector<int>(n + 1));
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= n; j++) {
-      cin >> a[i][j];
-    }
-  }
+	if(t == 1){
+		for(int i = 1;i <= n;i++){
+			cout << degIn[i] << " " << degOut[i] << "\n";
+		}
+	} else {
+		cout << n << "\n";
+		for(int i = 1;i <= n;i++){
+			cout << degOut[i] << " ";
+			for(int j = 1;j <= n;j++){
+				if(a[i][j]) cout << j << " ";
+			}
+			cout << "\n";
+		}
+	}
 
-  if (t == 1) {
-    for (int i = 1; i <= n; i++) {
-      int deg_in = 0, deg_out = 0;
-      for (int j = 1; j <= n; j++) {
-        if (a[i][j] == 1)
-          deg_out++;
-        if (a[j][i] == 1)
-          deg_in++;
-      }
-      cout << deg_in << " " << deg_out << "\n";
-    }
-  } else if (t == 2) {
-    cout << n << "\n";
-    for (int i = 1; i <= n; i++) {
-      vector<int> adj;
-      for (int j = 1; j <= n; j++) {
-        if (a[i][j] == 1)
-          adj.push_back(j);
-      }
-      cout << adj.size();
-      for (int v : adj)
-        cout << " " << v;
-      cout << "\n";
-    }
-  }
-
-  return 0;
+	return 0;
 }
