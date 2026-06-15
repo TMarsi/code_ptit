95a0f5aa15e82d0bbd28ac69f3de2bc69b9f3e26
TMarsi <122539045+TMarsi@users.noreply.github.com>
2026-06-15 21:59:30 +0700

    Update TRR1012.cpp

--- Toán rời rạc 2/TRR1012.cpp
+++ Toán rời rạc 2/TRR1012.cpp
@@ -1,42 +1,43 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    freopen("DT.INP", "r", stdin);
-    freopen("DT.OUT", "w", stdout);
+int n,m,a[101][101],degIn[101],degOut[101];
+int main(){
+	freopen("DT.INP","r",stdin);
+	freopen("DT.OUT","w",stdout);
 
-    int t, n;
-    cin >> t >> n;
+	int t;cin >> t;
+	cin >> n;
+	for(int i = 1;i <= n;i++){
+		for(int j = 1;j <= n;j++){
+			cin >> a[i][j];
+			if(a[i][j]){
+				degOut[i]++;
+				degIn[j]++;
+			}
+		}
+	}
 
-    vector<vector<int>> a(n+1, vector<int>(n+1, 0));
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            cin >> a[i][j];
+	if(t == 1){
+		for(int i = 1;i <= n;i++){
+			cout << degIn[i] << " " << degOut[i] << "\n";
+		}
+	} else {
+		for(int i = 1;i <= n;i++){
+			for(int j = 1;j <= n;j++){
+				if(a[i][j]) {
+					m++;
+				}
+			}
+		}
 
-    
-    
-    vector<int> indeg(n+1, 0), outdeg(n+1, 0);
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            if (a[i][j]) {
-                outdeg[i]++;
-                indeg[j]++;
-            }
+		cout << n << " " << m << "\n";
+		for(int i = 1;i <= m;i++){
+			for(int j = 1;j <= n;j++){
+				if(a[i][j]) cout << i << " " << j << "\n"; 
+			}
+		}
+	}
 
-    if (t == 1) {
-        for (int i = 1; i <= n; i++)
-            cout << indeg[i] << " " << outdeg[i] << "\n";
-    } else {
-        
-        vector<pair<int,int>> edges;
-        for (int u = 1; u <= n; u++)
-            for (int v = 1; v <= n; v++)
-                if (a[u][v])
-                    edges.push_back({u, v});
-        sort(edges.begin(), edges.end());
-        cout << n << " " << edges.size() << "\n";
-        for (auto& e : edges)
-            cout << e.first << " " << e.second << "\n";
-    }
-    return 0;
+	return 0;
 }
