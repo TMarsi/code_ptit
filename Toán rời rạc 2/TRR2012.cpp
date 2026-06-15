8f988df4d8ec3978f49aa0f1e8861715b6c73c8e
TMarsi <122539045+TMarsi@users.noreply.github.com>
2026-06-15 21:51:34 +0700

    Update TRR2012.cpp

--- Toán rời rạc 2/TRR2012.cpp
+++ Toán rời rạc 2/TRR2012.cpp
@@ -1,49 +1,53 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    freopen("TK.INP", "r", stdin);
-    freopen("TK.OUT", "w", stdout);
+int n,a[101][101];
+vector<int> edges;
+bool vs[101];
 
-    int n;
-    cin >> n;
-    vector<vector<int>> a(n+1, vector<int>(n+1, 0));
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            cin >> a[i][j];
+void BFS(int start){
+	queue<int> q;
+	q.push(start);
+	vs[start] = true;
+	edges.push_back(start);
+	while(!q.empty()){
+		int x = q.front();
+		q.pop();
+		for(int i = 1;i <= n;i++){
+			if(a[x][i] == 1 && !vs[i]){
+				vs[i] = true;
+				q.push(i);
+				edges.push_back(i);
+			}
+		}
+	}
+}
 
-    vector<bool> visited(n+1, false);
-    vector<vector<int>> components;
 
-    for (int i = 1; i <= n; i++) {
-        if (!visited[i]) {
-            
-            vector<int> comp;
-            queue<int> q;
-            q.push(i);
-            visited[i] = true;
-            while (!q.empty()) {
-                int u = q.front(); q.pop();
-                comp.push_back(u);
-                for (int v = 1; v <= n; v++) {
-                    if (a[u][v] && !visited[v]) {
-                        visited[v] = true;
-                        q.push(v);
-                    }
-                }
-            }
-            sort(comp.begin(), comp.end());
-            components.push_back(comp);
-        }
-    }
+int main(){
+	freopen("TK.INP","r",stdin);
+	freopen("TK.OUT","w",stdout);
 
-    cout << components.size() << "\n";
-    for (auto& c : components) {
-        for (int i = 0; i < (int)c.size(); i++) {
-            if (i > 0) cout << " ";
-            cout << c[i];
-        }
-        cout << "\n";
-    }
-    return 0;
+	cin >> n;
+	for(int i = 1;i <= n;i++){
+		for(int j = 1;j <= n;j++) cin >> a[i][j];
+	}
+
+	vector<vector<int>> luu;
+
+	for(int i = 1;i <= n;i++){
+		if(!vs[i]){
+			edges.clear();
+			BFS(i);
+			sort(edges.begin(),edges.end());
+			luu.push_back(edges);
+		}
+	}
+	cout << luu.size() << "\n";
+	for(int i = 0;i < (int)luu.size();i++){
+		for(int j = 0;j < (int)luu[i].size();j++){
+			cout << luu[i][j] << " ";
+		}
+		cout << "\n";
+	}
 }
