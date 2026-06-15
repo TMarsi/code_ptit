1cbe6bff00f2c74d90943d6073f601fe89f914dd
TMarsi <122539045+TMarsi@users.noreply.github.com>
2026-06-15 21:49:12 +0700

    Update TRR3001.cpp

--- Toán rời rạc 2/TRR3001.cpp
+++ Toán rời rạc 2/TRR3001.cpp
@@ -1,90 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-vector<vector<int>> a;
+int n,u,a[101][101],deg[101];
+bool vs[101];
+vector<int> ans;
 
-bool isConnected() {
-    vector<bool> vis(n+1, false);
-    queue<int> q;
-    q.push(1);
-    vis[1] = true;
-    while (!q.empty()) {
-        int u = q.front(); q.pop();
-        for (int v = 1; v <= n; v++)
-            if (a[u][v] && !vis[v]) { vis[v] = true; q.push(v); }
-    }
-    for (int i = 1; i <= n; i++) if (!vis[i]) return false;
-    return true;
+void DFS(int x){
+	vs[x] = true;
+	for(int i = 1;i <= n;i++){
+		if(a[x][i] == 1 && !vs[i]){
+			DFS(i);
+		}
+	}
 }
 
-vector<int> hierholzer(int start) {
-    
-    vector<vector<int>> rem = a;
-    stack<int> st;
-    vector<int> circuit;
-    st.push(start);
-    while (!st.empty()) {
-        int u = st.top();
-        bool found = false;
-        for (int v = 1; v <= n; v++) {
-            if (rem[u][v] > 0) {
-                rem[u][v]--;
-                rem[v][u]--;
-                st.push(v);
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            circuit.push_back(u);
-            st.pop();
-        }
-    }
-    reverse(circuit.begin(), circuit.end());
-    return circuit;
+void find(int x){
+	for(int i = 1;i <= n;i++){
+		if(a[x][i] == 1){
+			a[x][i] = 0;
+			a[i][x] = 0;
+			find(i);
+		}
+	}
+	ans.push_back(x);
 }
 
-int main() {
-    freopen("CT.INP", "r", stdin);
-    freopen("CT.OUT", "w", stdout);
+int main(){
+	freopen("CT.INP","r",stdin);
+	freopen("CT.OUT","w",stdout);
 
-    int t;
-    cin >> t;
+	int t;cin >> t;
+	if(t == 1){
+		cin >> n;
+		for(int i = 1;i <= n;i++){
+			for(int j = 1;j <= n;j++) {
+				cin >> a[i][j];
+				if(a[i][j]) deg[i]++;
+			}
+		}
+		DFS(1);
+	    bool connected = true;
+	    for(int i = 1;i <= n;i++){
+	        if(!vs[i]){ connected = false; break; }
+	    }
 
-    if (t == 1) {
-        cin >> n;
-        a.assign(n+1, vector<int>(n+1, 0));
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= n; j++)
-                cin >> a[i][j];
+	    int cnt = 0;
+	    for(int i = 1;i <= n;i++){
+	        if(deg[i] % 2 != 0) cnt++;
+	    }
 
-        if (!isConnected()) { cout << 0 << "\n"; return 0; }
+	    if(!connected) cout << 0 << "\n";
+	    else if(cnt == 0) cout << 1 << "\n";
+	    else if(cnt == 2) cout << 2 << "\n";
+	    else cout << 0 << "\n";
+	} else {
+		cin >> n >> u;
+		for(int i = 1;i <= n;i++){
+			for(int j = 1;j <= n;j++){
+				cin >> a[i][j];
+			}
+		}
+		find(u);
+		reverse(ans.begin(),ans.end());
+		for(int i = 0;i < (int)ans.size();i++){
+			cout << ans[i] << (i < (int)ans.size() - 1 ? " " : "\n");
+		}
 
-        int oddCount = 0;
-        for (int i = 1; i <= n; i++) {
-            int deg = 0;
-            for (int j = 1; j <= n; j++) deg += a[i][j];
-            if (deg % 2 != 0) oddCount++;
-        }
+	}
 
-        if (oddCount == 0) cout << 1 << "\n";
-        else if (oddCount == 2) cout << 2 << "\n";
-        else cout << 0 << "\n";
-    } else {
-        int u;
-        cin >> n >> u;
-        a.assign(n+1, vector<int>(n+1, 0));
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= n; j++)
-                cin >> a[i][j];
-
-        vector<int> circuit = hierholzer(u);
-        for (int i = 0; i < (int)circuit.size(); i++) {
-            if (i > 0) cout << " ";
-            cout << circuit[i];
-        }
-        cout << "\n";
-    }
-    return 0;
 }
