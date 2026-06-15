d381c44511c3c36b404c2de3e2c48aec47ee37ed
TMarsi <122539045+TMarsi@users.noreply.github.com>
2026-06-15 21:47:37 +0700

    Update TRR3007.cpp

--- Toán rời rạc 2/TRR3007.cpp
+++ Toán rời rạc 2/TRR3007.cpp
@@ -1,108 +1,39 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int n, startV;
-int a[105][105];
-bool visited[105];
-int path[105];
-int cycleCount;
+int n, start, count, a[105][105], path[105];
+bool vs[105];
 
 void backtrack(int u, int depth) {
-  if (depth == n) {
-    if (a[u][startV]) {
-      for (int i = 0; i < n; i++) {
-        if (i > 0)
-          cout << " ";
-        cout << path[i];
-      }
-      cout << " " << startV << "\n";
-      cycleCount++;
+    if (depth == n) {
+        if (a[u][start]) {
+            for (int i = 0; i < n; i++) cout << path[i] << " ";
+            cout << start << "\n";
+            count++;
+        }
+        return;
     }
-    return;
-  }
-  for (int v = 1; v <= n; v++) {
-    if (a[u][v] && !visited[v]) {
-      visited[v] = true;
-      path[depth] = v;
-      backtrack(v, depth + 1);
-      visited[v] = false;
+    for (int v = 1; v <= n; v++) {
+        if (a[u][v] && !vs[v]) {
+            vs[v] = true; 
+            path[depth] = v;
+            backtrack(v, depth + 1);
+            vs[v] = false;
+        }
     }
-  }
 }
 
 int main() {
-  freopen("CT.INP", "r", stdin);
-  freopen("CT.OUT", "w", stdout);
+    freopen("CT.INP", "r", stdin);
+    freopen("CT.OUT", "w", stdout);
+    
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++) cin >> a[i][j];
 
-  cin >> n >> startV;
-  for (int i = 1; i <= n; i++)
-    for (int j = 1; j <= n; j++)
-      cin >> a[i][j];
-
-  visited[startV] = true;
-  path[0] = startV;
-
-  backtrack(startV, 1);
-
-  cout << cycleCount << "\n";
-  if (cycleCount == 0) {
-  }
-  return 0;
+    vs[start] = true; 
+    path[0] = start;
+    backtrack(start, 1);
+    
+    cout << count;
+    return 0;
 }
-
-// cách 2 nhanh hơn k
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// const int MAXN = 105;
-// int n, startV;
-// bitset<MAXN> adj[MAXN];
-// int path[MAXN];
-// int cycleCount;
-
-// void backtrack(int u, int depth, bitset<MAXN>& visited) {
-//     if (depth == n) {
-//         if (adj[u][startV]) {
-//             for (int i = 0; i < n; i++) {
-//                 if (i > 0) cout << " ";
-//                 cout << path[i];
-//             }
-//             cout << " " << startV << "\n";
-//             cycleCount++;
-//         }
-//         return;
-//     }
-
-//     bitset<MAXN> candidates = adj[u] & ~visited;
-//     for (int v = candidates._Find_first(); v < MAXN; v =
-//     candidates._Find_next(v)) {
-//         visited.set(v);
-//         path[depth] = v;
-//         backtrack(v, depth + 1, visited);
-//         visited.reset(v);
-//     }
-// }
-
-// int main() {
-//     freopen("CT.INP", "r", stdin);
-//     freopen("CT.OUT", "w", stdout);
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(NULL);
-
-//     cin >> n >> startV;
-//     for (int i = 1; i <= n; i++) {
-//         for (int j = 1; j <= n; j++) {
-//             int x; cin >> x;
-//             if (x) adj[i].set(j);
-//         }
-//     }
-
-//     bitset<MAXN> visited;
-//     visited.set(startV);
-//     path[0] = startV;
-
-//     backtrack(startV, 1, visited);
-
-//     cout << cycleCount << "\n";
-//     return 0;
-// }
